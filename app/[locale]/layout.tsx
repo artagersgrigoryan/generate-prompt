bfb05ca64ab3548a69bb31792f0931ed7f13b1bc
artagersgrigoryan <artagersgrigoryan@gmail.com>
2026-06-21 22:53:41 +0400

    tag manager v3

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -4,6 +4,7 @@ import { NextIntlClientProvider } from "next-intl";
 import { getMessages, setRequestLocale } from "next-intl/server";
 import { notFound } from "next/navigation";
 import { routing } from "@/i18n/routing";
+import Script from "next/script";
 import { Header } from "@/components/Header";
 import "../globals.css";
 
